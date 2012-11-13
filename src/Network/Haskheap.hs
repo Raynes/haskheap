de7101efdaa13e5b5621cb418682456dfdd63f2d
Anthony Grimes <i@raynes.me>
2012-11-13 06:02:27 -0600

    First commit.

---
+++ src/Network/Haskheap.hs
@@ -0,0 +1,70 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Network.Haskheap
+       ( Paste(..)
+       , getPaste
+       )
+where
+
+import Control.Applicative ((<*>), (<$>))
+import Control.Monad       (liftM)
+import Data.Time.Format    (parseTime)
+import Data.Time.Clock     (UTCTime)
+import System.Locale       (defaultTimeLocale)
+import Network.URI         (URI, parseURI)
+import Data.Aeson          ((.:), decode, FromJSON(..), Value(..))
+import Network.HTTP.Conduit
+import Network.HTTP.Types (renderSimpleQuery, SimpleQuery, Method, methodGet, methodPost)
+import Network (withSocketsDo)
+import qualified Data.ByteString.Lazy.Char8 as B
+import qualified Data.ByteString.Char8 as SB
+
+parseRHTime :: String -> Maybe UTCTime
+parseRHTime = parseTime defaultTimeLocale "%FT%X%QZ"
+
+data Paste = Paste { getLines    :: Integer
+                   , getDate     :: Maybe UTCTime
+                   , getID       :: String
+                   , getLanguage :: String
+                   , getPrivate  :: Bool
+                   , getURL      :: Maybe URI
+                   , getUser     :: String
+                   , getBody     :: String
+                   } deriving (Show)
+
+instance FromJSON Paste where
+  parseJSON (Object v) =
+    Paste <$>
+    (v .: "lines")                    <*>
+    (liftM parseRHTime (v .: "date")) <*>
+    (v .: "paste-id")                 <*>
+    (v .: "language")                 <*>
+    (v .: "private")                  <*>
+    (liftM parseURI (v .: "url"))     <*>
+    (v .: "user")                     <*>
+    (v .: "contents")
+
+decodePaste :: B.ByteString -> Maybe Paste
+decodePaste s = decode s
+
+refheap :: String
+refheap = "https://www.refheap.com/api"
+
+refheapReq :: Method -> String -> Maybe SimpleQuery -> Maybe SimpleQuery -> IO B.ByteString
+refheapReq method path query body = do
+  let queryStr = renderSimpleQuery True <$> query
+      url      = refheap ++ path
+  req <- parseUrl $ case queryStr of
+    Just s  -> url ++ SB.unpack s
+    Nothing -> url
+  withSocketsDo $ do
+    let req'  = req { method = method }
+        req'' = case body of
+          Just b  -> urlEncodedBody b req'
+          Nothing -> req'
+    responseBody <$> withManager (httpLbs req'')
+
+getPaste :: String -> Maybe SimpleQuery -> IO Paste
+getPaste id auth = do
+  s <- refheapReq methodGet ("/paste/" ++ id) auth Nothing
+  let (Just p) = decodePaste s
+  return p
