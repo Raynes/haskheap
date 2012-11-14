601d2bec9b6f168917eab6ac2794fd3e05624c9e
Anthony Grimes <i@raynes.me>
2012-11-14 00:09:19 -0600

    Added a createPaste function, some type synonyms, and functions for making things easier.

--- src/Network/Haskheap.hs
+++ src/Network/Haskheap.hs
@@ -11,24 +11,37 @@ import Data.Time.Format    (parseTime)
 import Data.Time.Clock     (UTCTime)
 import System.Locale       (defaultTimeLocale)
 import Network.URI         (URI, parseURI)
-import Data.Aeson          ((.:), decode, FromJSON(..), Value(..))
+import Data.Aeson          ((.:), (.:?), decode, FromJSON(..), Value(..))
 import Network.HTTP.Conduit
 import Network.HTTP.Types (renderSimpleQuery, SimpleQuery, Method, methodGet, methodPost)
 import Network (withSocketsDo)
+import Control.Arrow ((***))
 import qualified Data.ByteString.Lazy.Char8 as B
 import qualified Data.ByteString.Char8 as SB
 
 parseRHTime :: String -> Maybe UTCTime
 parseRHTime = parseTime defaultTimeLocale "%FT%X%QZ"
 
+type PasteID  = String
+type Language = String
+type Contents = String
+type Query    = [(String, String)]
+type Auth     = (String, String)
+
+packQuery :: Query -> SimpleQuery
+packQuery = map (SB.pack *** SB.pack)
+
+composeAuth :: Auth -> Query
+composeAuth (user, token) = [("username", user), ("token", token)]
+
 data Paste = Paste { getLines    :: Integer
                    , getDate     :: Maybe UTCTime
-                   , getID       :: String
-                   , getLanguage :: String
+                   , getID       :: PasteID
+                   , getLanguage :: Language
                    , getPrivate  :: Bool
                    , getURL      :: Maybe URI
-                   , getUser     :: String
-                   , getBody     :: String
+                   , getUser     :: Maybe String
+                   , getBody     :: Contents
                    } deriving (Show)
 
 instance FromJSON Paste where
@@ -40,7 +53,7 @@ instance FromJSON Paste where
     (v .: "language")                 <*>
     (v .: "private")                  <*>
     (liftM parseURI (v .: "url"))     <*>
-    (v .: "user")                     <*>
+    (v .:? "user")                    <*>
     (v .: "contents")
 
 decodePaste :: B.ByteString -> Maybe Paste
@@ -49,9 +62,9 @@ decodePaste s = decode s
 refheap :: String
 refheap = "https://www.refheap.com/api"
 
-refheapReq :: Method -> String -> Maybe SimpleQuery -> Maybe SimpleQuery -> IO B.ByteString
+refheapReq :: Method -> String -> Maybe Query -> Maybe Query -> IO B.ByteString
 refheapReq method path query body = do
-  let queryStr = renderSimpleQuery True <$> query
+  let queryStr = renderSimpleQuery True . packQuery <$> query
       url      = refheap ++ path
   req <- parseUrl $ case queryStr of
     Just s  -> url ++ SB.unpack s
@@ -59,12 +72,19 @@ refheapReq method path query body = do
   withSocketsDo $ do
     let req'  = req { method = method, checkStatus = \_ _ -> Nothing }
         req'' = case body of
-          Just b  -> urlEncodedBody b req'
+          Just b  -> urlEncodedBody (packQuery b) req'
           Nothing -> req'
     responseBody <$> withManager (httpLbs req'')
 
 -- | Get a paste from refheap. Will return IO Nothing if the paste doesn't exist.
-getPaste :: String -> IO (Maybe Paste)
+getPaste :: PasteID -> IO (Maybe Paste)
 getPaste id = do
   s <- refheapReq methodGet ("/paste/" ++ id) Nothing Nothing
+  return $ decodePaste s
+
+createPaste :: Contents -> Bool -> Language -> Maybe Auth -> IO (Maybe Paste)
+createPaste body private language auth = do
+  s <- refheapReq methodPost "/paste" (composeAuth <$> auth) (Just [("contents", body)
+                                                                   ,("private", show private)
+                                                                   ,("language", language)])
   return $ decodePaste s
